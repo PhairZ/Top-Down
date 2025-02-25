613af69d762848a118131b61f09105f063cb31d4
Saif El-Din Ahmed <123702514+PhairZ@users.noreply.github.com>
2025-02-25 16:06:16 +0000

    did some basics

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 
 
+struct Vector2 {
+    int x, y;
+
+    Vector2(int p_x = 0, int p_y = 0) {
+        x = p_x, y = p_y;
+    }
+};
+
+
+enum ObjectType {
+    NONE,
+    PLAYER,
+    WALL,
+    COIN,
+};
+
+
+struct GameObject {
+    Vector2 position;
+    ObjectType type;
+
+    GameObject(Vector2 p_pos = Vector2(), ObjectType p_type = NONE) {
+        position = p_pos;
+        type = p_type;
+    }
+};
+
+
 int32_t main(int argc, char const *argv[])
 {
-    
-    printf("Hello, World!/n");
+    GameObject test_object = GameObject(Vector2(6, 9), PLAYER);
+    printf("%d", test_object.position.x);
 
     return 0;
 }
